03ca73ca0e3a963940cdd6c821674541cd6247e0
BasToTheMax <info@bastothemax.nl>
2023-10-01 20:30:03 +0200

    Some scripts

--- buildenv/run.sh
+++ buildenv/run.sh
@@ -1,1 +1,4 @@
-docker run --rm -it -v "$(pwd)":/root/env myos-buildenv
+docker run --rm -it -v "$(pwd)":/root/env myos-buildenv bash
+echo "build done!"
+sleep 3
+qemu-system-x86_64 -cdrom dist/x86_64/kernel.iso

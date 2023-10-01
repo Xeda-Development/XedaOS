16734c8db1444c6acae885d1c1662e78b0d4b996
BasToTheMax <info@bastothemax.nl>
2023-10-01 20:36:20 +0200

    Customize boot message

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,6 +2,10 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+
+    print_str("Welcome!");
+    print_newline();
+    print_newline();
+    print_str("Loading XedaOS...");
 }
